22b48fc10c53892c620f1527f8557b3cfdcc2de0
Tyler Rockwood <rockwotj@gmail.com>
2020-12-26 14:29:28 -0600

    Allow listing for files

--- webdav.go
+++ webdav.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"golang.org/x/net/webdav"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func NewWebDavServer(root string) http.Handler {
+	return &WebDavHandler{
+		fs: http.FileServer(http.Dir(root)),
+		dav: &webdav.Handler{
+			Prefix:     "",
+			FileSystem: webdav.Dir(root),
+			LockSystem: webdav.NewMemLS(),
+			Logger:     nil,
+		},
+	}
+}
 
-	fmt.Fprintf(w, "hello\n")
+type WebDavHandler struct {
+	fs  http.Handler
+	dav http.Handler
+}
+
+func (h *WebDavHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// NOTE: The builtin webdav server doesn't list directories, so we do that
+	// by using a plain HTTP Server
+	if r.Method == "GET" || r.Method == "HEAD" {
+		h.fs.ServeHTTP(w, r)
+	} else {
+		h.dav.ServeHTTP(w, r)
+	}
 }
 
 func main() {
-	handler := &webdav.Handler{
-		Prefix:     "",
-		FileSystem: webdav.Dir("/tmp/"),
-		LockSystem: webdav.NewMemLS(),
-		Logger:     nil,
-	}
-	http.ListenAndServe(":8090", handler)
+	http.ListenAndServe(":8090", NewWebDavServer("/tmp/"))
 }
